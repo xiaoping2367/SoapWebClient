bd2d0fd8874746728f67e18ec6b8b39896615658
mxiamxia <mxiamxia@gmail.com>
2014-08-13 17:05:24 -0400

    controller

--- soap-web-client/src/main/java/com/minxia/controller/SoapController.java
+++ soap-web-client/src/main/java/com/minxia/controller/SoapController.java
@@ -1,8 +1,16 @@
 package com.minxia.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SoapController {
+	
+	@RequestMapping(value = "/sendForm", method = RequestMethod.GET)
+	public String getResponse(){
+		return null;
+	}
+	
 
 }
